8dbe6764df90b21fc66b4e34e2b23661b113fc35
Krystal Foster <kmfoster777@gmail.com>
2018-11-09 11:54:42 -0800

    hardcode donate section

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -3,7 +3,32 @@ import React from "react";
 function Donate(){
   return(
     <div>
-    <p>This is the donate section!</p>
+    <div>
+		<div>
+      <p><em>Brain Pickings</em> remains free (and ad-free) and takes me hundreds of hours a month to research and write, and thousands of dollars to sustain.</p>
+      <p>If you find any joy and value in what I do, please consider becoming a Sustaining Patron with a recurring monthly donation of your choosing, between a cup of tea and a good lunch:</p>
+
+      <h4><strong>MONTHLY DONATION</strong></h4>
+
+      <div>
+        <p>♥ $3 / month</p>
+        <p>♥ $5 / month</p>
+        <p>♥ $7 / month</p>
+        <p>♥ $10 / month</p>
+        <p>♥ $25 / month</p>
+      </div>
+
+      <h4><a href="https://www.brainpickings.org/donate/">START NOW</a></h4>
+
+      <h4><strong>ONE-TIME DONATION</strong></h4>
+
+			<p>You can also become a Spontaneous Supporter with a one-time donation in any amount:</p>
+
+			<h4><a href="https://www.brainpickings.org/donate/">GIVE NOW</a></h4>
+
+      <p>Need to cancel a recurring donation? <a href="https://www.brainpickings.org/cancel-recurring-donation/"><strong>Go here</strong></a>.</p>
+		</div>
+</div>
     </div>
   );
 }
