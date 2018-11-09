defc800d14b34dfa093d7f2b72374bca89b87a7a
Krystal Foster <kmfoster777@gmail.com>
2018-11-09 14:06:03 -0800

    add global styles and background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,42 @@ import { Switch, Route } from 'react-router-dom';
 import RouteTest from "./RouteTest";
 
 function App(){
+  var mainDiv = {
+    width: 970,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    background: 'white',
+    padding: '1em',
+  }
   return (
-    <div>
+    <div style={mainDiv}>
+    <style global jsx>{`
+        body {
+          margin: 0;
+          background-image: url('https://maydesigns.cdn.prismic.io/maydesigns/e618d7ea9a430b4eacb60fbe94a78b867c145cef_page1.png');
+          background-repeat: repeat;
+          background-size: 260px auto;
+        }
+        h1 a:hover {
+          background: #262626;
+          color: #ffdb00;
+          box-shadow: -10px 0px 0px #262626, 10px 0px 0px #262626;
+        }
+
+        h1 a:after {
+            position: relative;
+            left: 10px;
+            line-height: 1.6em;
+            background: #ffdb00
+          }
+          h1 a {
+            line-height: 1.6em;
+            color: black;
+            background: #ffdb00;
+            text-decoration: none;
+            box-shadow: -10px 0px 0px #ffdb00, 10px 0px 0px #ffdb00;
+          }
+    `}</style>
       <Header/>
       <Switch>
         <Route exact path='/' component={MainArticle} />
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 function MainContent(){
+  var mainContentStyles = {
+    padding: '2em',
+  }
+  var titleStyles = {
+    margin: 0,
+    fontFamily: "'Fira Sans', sans-serif",
+  }
+  var quoteStyles = {
+    borderBottom: '1px solid #d9d9d9',
+    padding: '0 1.5em 1em',
+    fontSize: '1.25em',
+    lineHeight: '1.4em',
+  }
+  var authorStyles = {
+    color: '#b3b3b3',
+  }
   return (
-    <div class="main-articles">
-        <h1><a href="">A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan</a></h1>
-        <h2 class="opening-quote">“Out of such chaos, of such contradiction / We learn that we are neither devils nor divines…”</h2>
-        <p class="author-tag">BY MARIA POPOVA</p>
+    <div style={mainContentStyles}>
+        <h1 style={titleStyles}><a href="">A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan</a></h1>
+        <h2 style={quoteStyles}>“Out of such chaos, of such contradiction / We learn that we are neither devils nor divines…”</h2>
+        <p style={authorStyles}>BY MARIA POPOVA</p>
     </div>  
   );
 }
